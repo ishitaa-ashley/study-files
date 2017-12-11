fc81dea432d2f577bd05a084af93a822d976250d
Ishitaa Ashley <ishitaa.ashley4@gmail.com>
2017-12-11 18:05:27 +0530

    new commit

--- file2.js
+++ file2.js
@@ -2,4 +2,4 @@ console.log("first line in the second file");
 console.log("it says already uptodate file3");
 console.log("adding third line");
  //it is a  prt of checking
- //it is in the second branch
+ //it is in the new study2 branch
